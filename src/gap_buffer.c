1f0b0db9fdb4756a393de46837e292054ef48a00
Sean Rodman <srodman7689@gmail.com>
2018-04-26 20:49:49 -0400

    remove GapBuffer submodule. Merging GapBuffer and med repos.

--- src/gap_buffer.c
+++ src/gap_buffer.c
@@ -1,142 +1,89 @@
 #include "gap_buffer.h"
-#include "assert.h"
 
-gap_buffer *gap_buffer_create()
-{
-	gap_buffer *gb = malloc(sizeof(gap_buffer));
-	gb->size = DEFAULT_GAP_SIZE;
-	gb->buffer = malloc(sizeof(char) * gb->size);
-	gb->gap_start = gb->buffer;
-	gb->gap_end = (gb->gap_start + gb->size);
-	gb->buffer_end = gb->gap_end;
-	return gb;
+gap_buffer* new_gap_buffer(unsigned int size) {
+    gap_buffer* gb = malloc(sizeof(gap_buffer));
+    gb->buffer = malloc(sizeof(char) * size);
+    gb->gap_start = 0;
+    gb->gap_end = size;
+    gb->size = size; return gb;
 }
 
-static void gap_buffer_expand(gap_buffer *gb)
-{
-	int new_size = gb->size * 2;
-	char *new_buffer = malloc(sizeof(char) * new_size);
-
-	int before_len = gb->gap_start - gb->buffer;
-	memmove(new_buffer, gb->buffer, before_len);
-
-	int after_len = (gb->buffer_end - gb->gap_end);
-	memmove(new_buffer + (new_size - after_len), gb->gap_end, after_len);
-
-	gb->gap_start = new_buffer + before_len;
-	gb->gap_end = new_buffer + new_size - after_len;
-
-	free(gb->buffer);
-	gb->buffer = new_buffer;
-	gb->buffer_end = new_buffer + new_size;
-	gb->size = new_size;
+void free_gap_buffer(gap_buffer* gb) {
+    free(gb->buffer);
+    free(gb);
 }
 
-void gap_buffer_insert_char(gap_buffer *gb, char c)
-{
-	*gb->gap_start = c;
-	gb->gap_start++;
-	if (gb->gap_start == gb->gap_end)
-	{
-		gap_buffer_expand(gb);
-	};
+static void gap_buffer_grow(gap_buffer *gb) {
+    unsigned int new_size = gb->size * 2;
+
+    char * new_buffer = malloc(sizeof(char) * new_size);
+    memmove(new_buffer, gb->buffer, gb->gap_start);
+
+    unsigned int new_gap_end = new_size - (gb->size - gb->gap_end);
+    memmove(new_buffer + new_gap_end, gb->buffer + gb->gap_end, gb->size - gb->gap_end);
+
+    free(gb->buffer);
+
+    gb->buffer = new_buffer;
+    gb->gap_end = new_gap_end;
+    gb->size = gb->size * 2;
 }
 
-bool gap_buffer_backspace_char(gap_buffer *gb)
-{
-	if (gb->gap_start == gb->buffer)
-	{
-		return false;
-	}
+static void gap_buffer_move_gap(gap_buffer *gb, unsigned int position) {
+    if (position < gb->gap_start) {
+        unsigned int gap_delta = gb->gap_start - position;
 
-	gb->gap_start--;
-	return true;
+        gb->gap_start -= gap_delta;
+        gb->gap_end -= gap_delta;
+
+        memmove(gb->buffer + gb->gap_end, gb->buffer + gb->gap_start, gap_delta);
+    } else if (position > gb->gap_start) {
+        unsigned int gap_delta = position - gb->gap_start;
+
+        memmove(gb->buffer + gb->gap_start, gb->buffer + gb->gap_end, gap_delta);
+
+        gb->gap_start += gap_delta;
+        gb->gap_end += gap_delta;
+    }
 }
 
-bool gap_buffer_delete_char(gap_buffer *gb)
-{
-	if (gb->gap_end == gb->buffer + gb->size)
-	{
-		return false;
-	}
+void gap_buffer_insert_character_at_position(gap_buffer* gb, char character, unsigned int position) {
+    gap_buffer_move_gap(gb, position);
 
-	gb->gap_end++;
-	return true;
+    gb->buffer[gb->gap_start] = character;
+    gb->gap_start++;
+
+    if (gb->gap_start == gb->gap_end) {
+        gap_buffer_grow(gb);
+    }
 }
 
-bool gap_buffer_move_gap(gap_buffer *gb, int i)
-{
-	if (i == 0)
-	{
-		return false;
-	}
-
-	if (i > 0)
-	{
-		memmove(gb->gap_start, gb->gap_end, i);
-		gb->gap_start += i;
-		gb->gap_end += i;
-		return true;
-	}
-	else 
-	{
-                memmove(gb->gap_end + i, gb->gap_start + i, abs(i));
-		gb->gap_start += i;
-		gb->gap_end += i;
-		return true;
-	}	
+void gap_buffer_delete_back(gap_buffer* gb, unsigned int position) {
+    gap_buffer_move_gap(gb, position);
+    gb->gap_start--;
 }
 
-void gap_buffer_print(gap_buffer *gb)
-{
-	char *start;
-	for(start = gb->buffer; start < gb->gap_start; start++)
-	{
-		putchar(*start);
-	}
-
-	long numX;
-	for(numX = gb->gap_end - gb->gap_start; numX > 0; numX--)
-	{
-		putchar('x');
-	}
-
-	for(start = gb->gap_end; start < (gb->buffer + gb->size); start++)
-	{
-		putchar(*start);
-	}
-	putchar('\n');
+void gap_buffer_delete_forward(gap_buffer* gb, unsigned int position) {
+    gap_buffer_move_gap(gb, position);
+    gb->gap_end++;
 }
 
-char *gap_buffer_render(gap_buffer *gb)
-{
-	char *ret;
-	int ret_size = (gb->size - (gb->gap_end - gb->gap_start) + 1);
-	ret = malloc(sizeof(char) * ret_size);
-	memmove(ret, gb->buffer, (gb->gap_start - gb->buffer));
 
-	int after_len = (gb->buffer_end - gb->gap_end);
-	memmove(ret + (gb->size - after_len), gb->gap_end, after_len);
+void gap_buffer_debug_print(gap_buffer *gb) {
+    unsigned int i;
 
-	ret[ret_size-1] = '\0';
-	return ret;
+    for(i = 0; i < gb->gap_start; i++) {
+        printf("%c", gb->buffer[i]);
+    }
+
+    for (i = gb->gap_start; i < gb->gap_end; i++) {
+        printf("-");
+    }
+
+    for (i = gb->gap_end; i < gb->size; i++) {
+        printf("%c", gb->buffer[i]);
+    }
+
+    printf("\n");
 }
 
-bool gap_buffer_destroy(gap_buffer *gb)
-{
-	if (gb == NULL)
-       	{
-		return false;
-	}
-
-	if (gb->buffer == NULL)
-	{
-		free(gb);
-		return false;
-	}
-
-	free(gb->buffer);
-	free(gb);
-
-	return true;
-}
--- src/gap_buffer.h
+++ src/gap_buffer.h
@@ -1,36 +1,22 @@
-#ifndef GAP_BUFFER_H
-#define GAP_BUFFER_H
+#ifndef __GAP_BUFFER
+#define __GAP_BUFFER
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdbool.h>
 
-#define DEFAULT_GAP_SIZE 8
-
-typedef struct gap_buffer
-{
-	size_t size;
-	char *buffer;
-	char *gap_start;
-	char *gap_end;
-	char *buffer_end;
+typedef struct gap_buffer_t {
+    char* buffer;
+    unsigned int gap_start;
+    unsigned int gap_end;
+    unsigned int size;
 } gap_buffer;
 
-gap_buffer *gap_buffer_create();
-
-void gap_buffer_insert_char(gap_buffer *gb, char c);
-
-bool gap_buffer_backspace_char(gap_buffer *gb);
-
-bool gap_buffer_delete_char(gap_buffer *gb);
-
-bool gap_buffer_move_gap(gap_buffer *gb, int i);
-
-void gap_buffer_print(gap_buffer *gb);
-
-char *gap_buffer_render(gap_buffer *gb);
-
-bool gap_buffer_destroy(gap_buffer *gb);
+gap_buffer* new_gap_buffer(unsigned int size);
+void free_gap_buffer(gap_buffer *gb);
+void gap_buffer_insert_character_at_position(gap_buffer* gb, char character, unsigned int position);
+void gap_buffer_delete_back(gap_buffer* gb, unsigned int position);
+void gap_buffer_delete_forward(gap_buffer* gb, unsigned int position);
+void gap_buffer_debug_print(gap_buffer *gb);
 
 #endif
